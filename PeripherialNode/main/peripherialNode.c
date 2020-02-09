d1f19f82859058cbc37a1a12b2bdb139a01cb12e
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2020-02-09 22:11:46 +0200

    Changed ESP-IDF to 'stable' version (because of ESP-ADF) + change of code

--- PeripherialNode/main/peripherialNode.c
+++ PeripherialNode/main/peripherialNode.c
@@ -5,9 +5,7 @@
 #include "esp_wifi.h"
 #include "esp_system.h"
 #include "nvs_flash.h"
-#include "esp_event.h"
-#include "esp_netif.h"
-#include "protocol_examples_common.h"
+#include "esp_event_loop.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -21,138 +19,158 @@
 
 #include "esp_log.h"
 #include "mqtt_client.h"
+#include "tcpip_adapter.h"
 
-static const char *TAG = "PUBLISH_TEST";
+static const char *TAG = "PERIPHERIAL_NODE";
 
-static EventGroupHandle_t mqtt_event_group;
+static EventGroupHandle_t wifi_event_group;
 const static int CONNECTED_BIT = BIT0;
 
-static esp_mqtt_client_handle_t mqtt_client = NULL;
+char my_ipv4_addr[16];
 
-static char *expected_data = NULL;
-static char *actual_data = NULL;
-static size_t expected_size = 0;
-static size_t expected_published = 2;
-static size_t actual_published = 0;
-static int qos_test = 0;
+static void network_to_ip_str(char *network_byte_order)
+{
+    int nbo_len = 8;
+    char octet_hex[5];
+	char octet_dec[5];
+	char *sep = ".";
+
+	strcpy(my_ipv4_addr, "");
+	
+	for (int i = nbo_len - 2; i >= 0; i-=2)
+	{
+		sprintf(octet_hex, "%c%c", network_byte_order[i], network_byte_order[i+1]);
+		if(i == 0)
+		{
+			sep = "";
+		}
+		sprintf(octet_dec, "%d%s", (int)strtol(octet_hex, NULL, 16), sep);
+		strcat(my_ipv4_addr, octet_dec);
+	}
+}
 
 static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
 {
     esp_mqtt_client_handle_t client = event->client;
-    static int msg_id = 0;
-    static int actual_len = 0;
+    int msg_id;
+    char *topic = CONFIG_TOPIC;
+    char message[256];
+    int qos = 1;
+
+    tcpip_adapter_ip_info_t ipInfo; 
+    tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_STA, &ipInfo);
+    
+    /* send ip address in network byte order */
+    char ip_network_byte_order[8];
+    sprintf(ip_network_byte_order, "%x", ipInfo.ip.addr);
+
+    network_to_ip_str(ip_network_byte_order);
+
+    strcpy(message, my_ipv4_addr);
+
     switch (event->event_id) {
-    case MQTT_EVENT_CONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        xEventGroupSetBits(mqtt_event_group, CONNECTED_BIT);
-        msg_id = esp_mqtt_client_subscribe(client, CONFIG_SUBSCIBE_TOPIC, qos_test);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+        case MQTT_EVENT_CONNECTED:
+            ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+            
+            msg_id = esp_mqtt_client_subscribe(client, CONFIG_TOPIC, qos);
+            ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+            break;
 
-        break;
-    case MQTT_EVENT_DISCONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-        break;
+        case MQTT_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+            break;
 
-    case MQTT_EVENT_SUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_UNSUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_PUBLISHED:
-        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_DATA:
-        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-        printf("DATA=%.*s\r\n", event->data_len, event->data);
-        printf("ID=%d, total_len=%d, data_len=%d, current_data_offset=%d\n", event->msg_id, event->total_data_len, event->data_len, event->current_data_offset);
-        if (event->topic) {
-            actual_len = event->data_len;
-            msg_id = event->msg_id;
-        } else {
-            actual_len += event->data_len;
+        case MQTT_EVENT_SUBSCRIBED:
+            ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
 
-            /* check consisency with msg_id across multiple data events for single msg */
-            if (msg_id != event->msg_id) {
-                ESP_LOGI(TAG, "Wrong msg_id in chunked message %d != %d", msg_id, event->msg_id);
-                abort();
-            }
-        }
-        memcpy(actual_data + event->current_data_offset, event->data, event->data_len);
-        if (actual_len == event->total_data_len) {
-            if (0 == memcmp(actual_data, expected_data, expected_size)) {
-                printf("OK!");
-                memset(actual_data, 0, expected_size);
-                actual_published ++;
-                if (actual_published == expected_published) {
-                    printf("Correct pattern received exactly x times\n");
-                    ESP_LOGI(TAG, "Test finished correctly!");
-                }
-            } else {
-                printf("FAILED!");
-                abort();
-            }
-        }
-        break;
-    case MQTT_EVENT_ERROR:
-        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-        break;
-    default:
-        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
-        break;
+            msg_id = esp_mqtt_client_publish(client, topic, message, 0, qos, 0);
+            ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+            break;
+
+        case MQTT_EVENT_UNSUBSCRIBED:
+            ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+            break;
+
+        case MQTT_EVENT_PUBLISHED:
+            ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+            break;
+
+        case MQTT_EVENT_DATA:
+            ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+            printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+            printf("DATA=%.*s\r\n", event->data_len, event->data);
+            break;
+
+        case MQTT_EVENT_ERROR:
+            ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+            break;
+            
+        default:
+            ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+            break;
     }
     return ESP_OK;
 }
 
-static void mqtt_app_start(void)
+static esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
 {
-    mqtt_event_group = xEventGroupCreate();
-    const esp_mqtt_client_config_t mqtt_cfg = {
-        .event_handle = mqtt_event_handler
-    };
+    switch (event->event_id) {
+        case SYSTEM_EVENT_STA_START:
+            esp_wifi_connect();
+            break;
+        case SYSTEM_EVENT_STA_GOT_IP:
+            xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
 
-    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
-    mqtt_client = esp_mqtt_client_init(&mqtt_cfg);
+            break;
+        case SYSTEM_EVENT_STA_DISCONNECTED:
+            esp_wifi_connect();
+            xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+            break;
+        default:
+            break;
+    }
+    return ESP_OK;
 }
 
-void app_main(void)
+static void wifi_init(void)
 {
-    char pattern[32] = "Hello!";
-    int repeat = 1;
-    expected_published = 1;
+    tcpip_adapter_init();
+    wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK(esp_event_loop_init(wifi_event_handler, NULL));
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = CONFIG_WIFI_SSID,
+            .password = CONFIG_WIFI_PASSWORD,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_LOGI(TAG, "start the WIFI SSID:[%s]", CONFIG_WIFI_SSID);
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_LOGI(TAG, "Waiting for wifi");
+    xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
+}
 
+static void mqtt_app_start(void)
+{
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .uri = CONFIG_BROKER_URL,
+        .event_handle = mqtt_event_handler,
+    };
+
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_start(client);
+}
+
+void app_main()
+{
     esp_log_level_set("*", ESP_LOG_INFO);
     esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
 
-    ESP_ERROR_CHECK(nvs_flash_init());
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    /* upper example wifi auth */
-    ESP_ERROR_CHECK(example_connect());
-
-    //ESP_LOGW("IP", );
-
+    nvs_flash_init();
+    wifi_init();
     mqtt_app_start();
-
-    ESP_LOGI(TAG, "PATTERN:%s REPEATED:%d PUBLISHED:%d\n", pattern, repeat, expected_published);
-
-    printf("EXPECTED STRING %.*s, SIZE:%d\n", expected_size, expected_data, expected_size);
-    esp_mqtt_client_stop(mqtt_client);
-
-    ESP_LOGI(TAG, "[TCP transport] Startup..");
-    esp_mqtt_client_set_uri(mqtt_client, CONFIG_BROKER_TCP_URI);
-    
-    xEventGroupClearBits(mqtt_event_group, CONNECTED_BIT);
-    esp_mqtt_client_start(mqtt_client);
-    ESP_LOGI(TAG, "Note free memory: %d bytes", esp_get_free_heap_size());
-    xEventGroupWaitBits(mqtt_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
-   
-    ESP_LOGW("test", "test-break");
-   
-    for (int i = 0; i < repeat; i++) {
-        int msg_id = esp_mqtt_client_publish(mqtt_client, CONFIG_PUBLISH_TOPIC, expected_data, expected_size, qos_test, 0);
-        ESP_LOGI(TAG, "[%d] Publishing...", msg_id);
-    }
 }
